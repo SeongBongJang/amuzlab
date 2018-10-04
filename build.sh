9b2876d5dea53f8b0fc1a94520570b2bcd50c407
seongbongjang <jangsb7113@naver.com>
2018-10-04 18:01:26 +0900

    docker file name 수정

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@
 docker version
 
 # build the image based on the Dockerfile
-docker build -t amuz_base:0.1 -f Dockerfile.amuz_base .
+docker build -t amuz_shot_solution:0.1 .
 # confirm image is present
 docker images
 # enter container terminal
-docker run --name=amuz_base1 -d --privileged=true --net=host --runtime=nvidia -it -v /home/seongbong/amuz_nas/VODMV:/home/amuz amuz_base:0.1 /usr/sbin/init
+docker run --name=amuz_shot_solution -d --privileged=true --net=host --runtime=nvidia -it -v /home/seongbong/amuz_nas/VODMV:/home/amuz amuz_shot_solution:0.1 /usr/sbin/init
 
 # build the image based on the Dockerfile
 #docker build -t amuz_base:0.2 -f Dockerfile.amuz_base2 .
@@ -17,6 +17,6 @@ docker run --name=amuz_base1 -d --privileged=true --net=host --runtime=nvidia -i
 # enter container terminal
 #docker run --name=amuz_base -d --privileged=true --net=host --runtime=nvidia -it -v /home/seongbong/amuz_nas/VODMV:/home/amuz amuz_base:0.2 /usr/sbin/init
 
-docker exec -it amuz_base1 /bin/bash
+docker exec -it amuz_shot_solution /bin/bash
 
 
